489b6a9008a891812b51c74948fa88e9e0d6f3c9
Vladimir Tonkonogov <tonk0006@algonquinlive.com>
2015-12-03 19:52:56 -0500

    added grunfile.js & ran grunt default to create dist folder + files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,83 +1,106 @@
 /*global module:false*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['lib/<%= pkg.name %>.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        // Task configuration.
+        concat: {
+            options: {
+                separator: '\n/************************* START OF NEW FILE ************************/\n\n',
+            },
+            js: {
+                src: ['bower_components/bootstrap/dist/js/bootstrap.js', 'bower_components/jquery/dist/jquery.js', 'bower_components/bootstrap/dist/js/main.js'],
+                dest: 'dist/js/main.js'
+            },
+            css: {
+                src: ['bower_components/bootstrap/dist/css/bootstrap.css', 'bower_components/font-awesome/css/font-awesome.css', 'bower_components/bootstrap/dist/css/main.css'],
+                dest: 'dist/css/main.css'
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'bower_components/font-awesome/fonts',
+                        src: '*',
+                        dest: 'dist/fonts/'
+					},
+                    {
+                        expand: true,
+                        cwd: 'bower_components/bootstrap/dist/img',
+                        src: '*',
+                        dest: 'dist/img/'
+					},
+//                    {
+//                        src: 'bower_components/bootstrap/dist/css/bootstrap.css',
+//                        dest: 'dist/css/bootstrap.css'
+//					},
+//                    {
+//                        src: 'bower_components/bootstrap/dist/css/bootstrap.js',
+//                        dest: 'bootstrap/dist/js/bootstrap.js',
+//                    },
+//                    {
+//                        src: 'bower_components/font-awesome/css/font-awesome.css',
+//                        dest: 'dist/css/font-awesome.css'
+//					},
+//                    {
+//                        src: 'bower_components/jquery/dist/jquery.js',
+//                        dest: 'dist/js/jquery.js'
+//					},
+//                    {
+//                        src: 'bower_components/bootstrap/dist/css/main.css',
+//                        dest: 'dist/css/main.css'
+//					},
+//                    {
+//                        src: 'bower_components/bootstrap/dist/js/main.js',
+//                        dest: 'dist/js/main.js'
+//					},
+                    {
+                        src: 'bower_components/bootstrap/dist/index.html',
+                        dest: 'dist/index.html'
+					},
+     			],
+            },
+        },
+        uglify: {
+            dist: {
+                src: 'dist/js/main.js',
+                dest: 'dist/js/main.min.js'
+            }
+        },
+        cssmin: {
+            target: {
+                files: {
+                    "dist/css/main.min.css": 'dist/css/*.css'
+                }
+            }
+        },
+        //        clean: {
+        //            css: ['dist/css/*.css', '!dist/css/*.min.css'],
+        //            js: ['dist/js/*.js', '!dist/js/*.min.js']
+        //        }
+        clean: {
+            css: 'dist/css/main.css',
+            js: 'dist/js/main.js'
         }
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
-      }
-    }
-  });
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+    // Default task.
+    grunt.registerTask('default', ['concat', 'copy', 'uglify', 'cssmin', 'clean']);
 
 };
